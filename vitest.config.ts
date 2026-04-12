d59a639d5274d5e3e38031d89be2f57964045f06
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-04-13 06:57:07 +0700

    chore: updated config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,49 +1,9 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { defineConfig } from "vitest/config";
 
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-
-import { playwright } from "@vitest/browser-playwright";
-
-const dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   test: {
-    projects: [
-      // 🧠 1️⃣ UNIT TEST PROJECT (domain, logic, etc)
-      {
-        test: {
-          name: "unit",
-          environment: "node",
-          include: ["src/**/*.test.ts"],
-        },
-      },
-
-      // 🖥 2️⃣ STORYBOOK PROJECT
-      {
-        extends: true,
-        plugins: [
-          storybookTest({
-            configDir: path.join(dirname, ".storybook"),
-          }),
-        ],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [{ browser: "chromium" }],
-          },
-          setupFiles: [".storybook/vitest.setup.ts"],
-        },
-      },
-    ],
+    environment: "jsdom",
+    globals: true,
+    include: ["src/**/*.test.{ts,tsx}"],
   },
 });
