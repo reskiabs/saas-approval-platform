99833b6c184ab478b2be441751c2044ff96287df
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-04-29 21:17:34 +0700

    added proxy

---
+++ src/proxy.ts
@@ -0,0 +1,38 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+function isAuthenticated(request: NextRequest) {
+  const accessToken = request.cookies.get("sb-access-token");
+  console.log("🚀 ~ isAuthenticated ~ accessToken:", accessToken);
+  const refreshToken = request.cookies.get("sb-refresh-token");
+  console.log("🚀 ~ isAuthenticated ~ refreshToken:", refreshToken);
+
+  return !!(accessToken || refreshToken);
+}
+
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const isLoggedIn = isAuthenticated(request);
+  console.log("🚀 ~ proxy ~ isLoggedIn:", isLoggedIn);
+
+  // route groups
+  const isAuthPage = pathname.startsWith("/");
+  const isProtectedPage = pathname.startsWith("/dashboard");
+
+  // 1. user belum login → akses protected
+  if (!isLoggedIn && isProtectedPage) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // 2. user sudah login → akses login page
+  if (isLoggedIn && isAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/dashboard/:path*"],
+};
